d91fea737dd28f397a9b527fdd7768a100269f10
cis_farhan-admin <fmamsocials@gmail.com>
2025-09-19 11:52:18 -0300

    I just added my assignment 1

---
+++ Assignments/cis2232_assignment1_farhan_farhan/src/main/java/ca/hccis/recordstore/Controller.java
@@ -0,0 +1,108 @@
+package ca.hccis.recordstore;
+
+import ca.hccis.recordstore.util.CisUtility;
+import com.google.gson.Gson;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.text.MessageFormat;
+import java.util.List;
+
+
+
+public class Controller {
+
+    public static final int EXIT = 0;
+    public static final String MENU = "1) Add Sale" + System.lineSeparator()
+            +"2) View Sales" + System.lineSeparator()
+            + EXIT + ") Exit"
+            + System.lineSeparator();
+    public static final String MESSAGE_ERROR = "Error";
+    public static final String MESSAGE_EXIT = "Goodbye!";
+    public static final String MESSAGE_SUCCESS = "Success";
+    public static final String PATH = "C:\\CIS2232\\";
+    public static final String FILE_NAME = "data_farhan_farhan.json";
+
+    private static Path journalPath = Paths.get(PATH);
+    private static FileWriter journalWriter = null;
+
+    public static void main(String[] args) {
+
+        journalPath = Paths.get(PATH + FILE_NAME);
+
+//        // Creating a directory if it doesn't exist
+         File directory = new File(PATH);
+
+        if (!directory.exists()) {
+            directory.mkdir();
+        }
+
+        try {
+            journalWriter = new FileWriter(journalPath.toString(), true);
+        } catch (IOException e) {
+            System.out.println("Error opening file.");
+            throw new RuntimeException(e);
+        }
+
+        int menuOption;
+        do {
+            menuOption = CisUtility.getInputInt(MENU);
+            switch (menuOption) {
+                case 1:
+                    processAdd();
+                    break;
+                case 2:
+                    processShow();
+                    break;
+                case EXIT:
+                    System.out.println(MESSAGE_EXIT);
+                    break;
+                default:
+                    System.out.println(MESSAGE_ERROR);
+                    break;
+            }
+        } while (menuOption != EXIT);
+
+        try {
+            journalWriter.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static void processAdd() {
+        RecordSale sale = new RecordSale();
+        sale.getInformation();
+
+        try {
+            journalWriter.write(sale.toJson() + System.lineSeparator());
+            journalWriter.flush();
+        } catch (IOException e) {
+            System.out.println("Error writing to file.");
+        }
+    }
+
+    private static void processShow() {
+        Gson gson = new Gson();
+        try {
+            List<String> lines = Files.readAllLines(Paths.get(PATH + FILE_NAME));
+            if (lines.isEmpty()) {
+                System.out.println("No sales found.");
+            } else {
+                System.out.println("Here are the sales we found");
+                for (String line : lines) {
+                    RecordSale sale = gson.fromJson(line, RecordSale.class);
+                    System.out.println(sale);
+                    System.out.println("----------------------------");
+                }
+            }
+        } catch (IOException e) {
+            System.out.println("Error reading from file.");
+            throw new RuntimeException(e);
+        }
+    }
+}
--- Projects/record_store_sales_tracker/src/main/resources/db/mysql/createDatabase1.sql
+++ Projects/record_store_sales_tracker/src/main/resources/db/mysql/createDatabase1.sql
@@ -1,97 +1,54 @@
-DROP DATABASE IF EXISTS cis2232_performance_hall;
-CREATE DATABASE cis2232_performance_hall;
-use cis2232_performance_hall;
+DROP DATABASE IF EXISTS cis2232_record_store_sales_tracker;
+CREATE DATABASE cis2232_record_store;
+use cis2232_record_store;
 
-CREATE TABLE CodeType (codeTypeId int(3) COMMENT 'This is the primary key for code types',
-  englishDescription varchar(100) NOT NULL COMMENT 'English description',
-  frenchDescription varchar(100) DEFAULT NULL COMMENT 'French description',
-  createdDateTime datetime DEFAULT NULL,
-  createdUserId varchar(20) DEFAULT NULL,
-  updatedDateTime datetime DEFAULT NULL,
-  updatedUserId varchar(20) DEFAULT NULL
-) COMMENT 'This tables holds the code types that are available for the application';
+--------------------------------------------------------------------------------
+-- Note the table below to hold data associated with your project.  Expect one
+-- table with 7-9 fields.
+--------------------------------------------------------------------------------
 
-INSERT INTO CodeType (CodeTypeId, englishDescription, frenchDescription, createdDateTime, createdUserId, updatedDateTime, updatedUserId) VALUES
-(1, 'User Types', 'User Types FR', sysdate(), '', CURRENT_TIMESTAMP, '');
-INSERT INTO CodeType (CodeTypeId, englishDescription, frenchDescription, createdDateTime, createdUserId, updatedDateTime, updatedUserId) VALUES
-(2, 'Ticket Types', 'Ticket Types FR', sysdate(), '', CURRENT_TIMESTAMP, '');
+CREATE TABLE SkillsAssessmentSquashTechnical
+(
+    id                int(5),
+    assessmentDate    varchar(10) NOT NULL COMMENT 'yyyy-MM-dd',
+    createdDateTime   varchar(20) NOT NULL COMMENT 'yyyy-MM-dd hh:mm:ss',
+    athleteName       varchar(50) NOT NULL COMMENT 'Athletes name',
+    assessorName      varchar(50) NOT NULL COMMENT 'Athletes name',
+    forehandDrives    int(5) COMMENT 'Number of forehand drives',
+    backhandDrives    int(5) COMMENT 'Number of backhand drives',
+    forehandVolleyMax int(5) COMMENT 'Max number of forehand volleys',
+    forehandVolleySum int(5) COMMENT 'Sum of forehand volleys',
+    backhandVolleyMax int(5) COMMENT 'Max number of backhand volleys',
+    backhandVolleySum int(5) COMMENT 'Sum of backhand volleys',
+    technicalScore    int(5) COMMENT 'Score calculated at submission'
+) COMMENT 'This table holds technical skills assessment details';
 
-CREATE TABLE CodeValue (
-  codeTypeId int(3) NOT NULL COMMENT 'see code_type table',
-  codeValueSequence int(3) NOT NULL,
-  englishDescription varchar(100) NOT NULL COMMENT 'English description',
-  englishDescriptionShort varchar(20) NOT NULL COMMENT 'English abbreviation for description',
-  frenchDescription varchar(100) DEFAULT NULL COMMENT 'French description',
-  frenchDescriptionShort varchar(20) DEFAULT NULL COMMENT 'French abbreviation for description',
-  sortOrder int(3) DEFAULT NULL COMMENT 'Sort order if applicable',
-  createdDateTime datetime DEFAULT NULL,
-  createdUserId varchar(20) DEFAULT NULL,
-  updatedDateTime datetime DEFAULT NULL,
-  updatedUserId varchar(20) DEFAULT NULL
-) COMMENT='This will hold code values for the application.';
+ALTER TABLE SkillsAssessmentSquashTechnical
+    ADD PRIMARY KEY (id);
+ALTER TABLE SkillsAssessmentSquashTechnical
+    MODIFY id int(4) NOT NULL AUTO_INCREMENT COMMENT 'This is the primary key',
+    AUTO_INCREMENT = 1;
 
-INSERT INTO CodeValue (codeTypeId, codeValueSequence, englishDescription, englishDescriptionShort, frenchDescription, frenchDescriptionShort, createdDateTime, createdUserId, updatedDateTime, updatedUserId) VALUES
-(1, 1, 'General', 'General', 'GeneralFR', 'GeneralFR', '2015-10-25 18:44:37', 'admin', '2015-10-25 18:44:37', 'admin');
-INSERT INTO CodeValue (codeTypeId, codeValueSequence, englishDescription, englishDescriptionShort, frenchDescription, frenchDescriptionShort, createdDateTime, createdUserId, updatedDateTime, updatedUserId) VALUES
-(1, 2, 'Admin', 'Admin', 'Admin', 'Admin', '2015-10-25 18:44:37', 'admin', '2015-10-25 18:44:37', 'admin');
+-- insert into SkillsAssessmentSquashTechnical values(0, '2022-08-22',	'2022-08-20 11:35:15','Maria Smith','BJ MacLean',		11,	5,	14,	78,	6,	59,0);
+-- insert into SkillsAssessmentSquashTechnical values(0, '2022-08-11',	'2022-08-11 11:35:15','Rhonda Jones','BJ MacLean',		5,	7,	4,	36,	5,	38,0);
+-- insert into SkillsAssessmentSquashTechnical values(0, '2022-08-07', '2022-08-07 11:35:15','Chad Collins','BJ MacLean',		8,	8,	4,	37,	5,	42,0);
+-- insert into SkillsAssessmentSquashTechnical values(0, '2022-08-07', '2022-08-07 11:35:15','Rhonda Jones','BJ MacLean',		12,	8,	9,	53,	4,	42,0);
+-- insert into SkillsAssessmentSquashTechnical values(0, '2022-08-07', '2022-08-05 11:35:15','Chad Collins','BJ MacLean',		8,	10,	7,	52,	3,	26,0);
+-- insert into SkillsAssessmentSquashTechnical values(0, '2022-08-08',	'2022-08-08 11:35:15','Rhonda Jones','BJ MacLean',		10,	8,	8,	61,	6,	57,0);
+-- insert into SkillsAssessmentSquashTechnical values(0, '2022-08-10',	'2022-08-10 11:35:15','Chad Collins','BJ MacLean',		17,	14,	8,	70,	13,	84,0);
+-- insert into SkillsAssessmentSquashTechnical values(0, '2022-08-08',	'2022-08-08 11:35:15','Maria Smith','BJ MacLean',		17,	18,	12,	77,	8,	63,0);
+-- insert into SkillsAssessmentSquashTechnical values(0, '2022-08-22',	'2022-08-20 11:35:15','Chad Collins','BJ MacLean',		14,	11,	10,	86,	16,	87,0);
 
-INSERT INTO CodeValue (codeTypeId, codeValueSequence, englishDescription, englishDescriptionShort, frenchDescription, frenchDescriptionShort, createdDateTime, createdUserId, updatedDateTime, updatedUserId) VALUES
-(2, 3, 'Balcony', 'Balcony', 'BalconyFR', 'BalconyFR', '2015-10-25 18:44:37', 'admin', '2015-10-25 18:44:37', 'admin');
-INSERT INTO CodeValue (codeTypeId, codeValueSequence, englishDescription, englishDescriptionShort, frenchDescription, frenchDescriptionShort, createdDateTime, createdUserId, updatedDateTime, updatedUserId) VALUES
-(2, 4, 'Orchestra', 'Orchestra', 'OrchestraFR', 'OrchestraFR', '2015-10-25 18:44:37', 'admin', '2015-10-25 18:44:37', 'admin');
-INSERT INTO CodeValue (codeTypeId, codeValueSequence, englishDescription, englishDescriptionShort, frenchDescription, frenchDescriptionShort, createdDateTime, createdUserId, updatedDateTime, updatedUserId) VALUES
-(2, 5, 'Main Floor', 'Main Floor', 'Main FloorFR', 'Main FloorFR', '2015-10-25 18:44:37', 'admin', '2015-10-25 18:44:37', 'admin');
+INSERT INTO SkillsAssessmentSquashTechnical (id, assessmentDate, createdDateTime, athleteName, assessorName,
+                                             forehandDrives, backhandDrives, forehandVolleyMax, forehandVolleySum,
+                                             backhandVolleyMax, backhandVolleySum, technicalScore)
+VALUES (1, '2022-08-22', '2023-11-07 01:38:48', 'Maria Smith', 'BJ MacLean', 11, 5, 14, 78, 6, 59, 1085),
+       (2, '2022-08-11', '2023-11-07 01:38:52', 'Rhonda Jones', 'BJ MacLean', 5, 7, 4, 36, 5, 38, 622),
+       (3, '2022-08-07', '2023-11-07 01:38:56', 'Chad Collins', 'BJ MacLean', 8, 8, 4, 37, 5, 42, 707),
+       (4, '2022-08-07', '2023-11-07 01:38:59', 'Rhonda Jones', 'BJ MacLean', 12, 8, 9, 53, 4, 42, 879),
+       (5, '2022-08-07', '2023-11-07 01:39:01', 'Chad Collins', 'BJ MacLean', 8, 10, 7, 52, 3, 26, 740),
+       (6, '2022-08-08', '2023-11-07 01:39:04', 'Rhonda Jones', 'BJ MacLean', 10, 8, 8, 61, 6, 57, 972),
+       (7, '2022-08-10', '2023-11-07 01:39:07', 'Chad Collins', 'BJ MacLean', 17, 14, 8, 70, 13, 84, 1403),
+       (8, '2022-08-08', '2023-11-07 01:39:10', 'Maria Smith', 'BJ MacLean', 17, 18, 12, 77, 8, 63, 1385),
+       (9, '2022-08-22', '2023-11-07 01:38:44', 'Chad Collins', 'BJ MacLean', 14, 11, 10, 86, 16, 87, 1448);
 
-
-
-CREATE TABLE UserAccess (
-  userAccessId int(3) NOT NULL,
-  username varchar(100) NOT NULL COMMENT 'Unique user name for app',
-  password varchar(128) NOT NULL,
-  name varchar(128),
-  userAccessStatusCode int(3) NOT NULL DEFAULT '1' COMMENT 'Code type #2',
-  userTypeCode int(3) NOT NULL DEFAULT '1' COMMENT 'Code type #1',
-  createdDateTime datetime DEFAULT NULL COMMENT 'When user was created.'
-);
-
-ALTER TABLE CodeType
-  ADD PRIMARY KEY (codeTypeId);
-
-ALTER TABLE CodeValue
-  ADD PRIMARY KEY (codeValueSequence);
---  ADD KEY codeTypeId (codeTypeId);
-
-ALTER TABLE UserAccess
-  ADD PRIMARY KEY (userAccessId),
-  ADD KEY userTypeCode (userTypeCode);
-
-ALTER TABLE CodeType
-  MODIFY codeTypeId int(3) NOT NULL COMMENT 'This is the primary key for code types';
-
-ALTER TABLE CodeValue
-  MODIFY codeValueSequence int(3) NOT NULL;
-
-ALTER TABLE UserAccess
-  MODIFY userAccessId int(3) NOT NULL AUTO_INCREMENT;
-
-CREATE TABLE TicketOrder(
-id int(5),
-customerName varchar(20) NOT NULL COMMENT 'Customer name',
-hollpassNumber int(5) NOT NULL  COMMENT 'Student Hollpass Number',
-dateOfOrder varchar(10) NOT NULL COMMENT 'Date of order',
-dateOfPerformance varchar(10) NOT NULL COMMENT 'Date of performance (yyyy-MM-dd)',
-timeOfPerformance varchar(5) NOT NULL COMMENT 'Time of performance (hh24:mm)',
-numberOfTickets int(4) NOT NULL  COMMENT 'Number of Tickets',
-ticketTypeCode int(3) NOT NULL default 0 COMMENT 'Type of ticket',
-costOfTickets decimal(6,2) NOT NULL  COMMENT 'Cost of Tickets'
-) COMMENT 'This table holds ticket order data';
-
-ALTER TABLE TicketOrder
-  ADD PRIMARY KEY (id);
-ALTER TABLE TicketOrder
-  MODIFY id int(4) NOT NULL AUTO_INCREMENT COMMENT 'This is the primary key', AUTO_INCREMENT=1;
-
-insert into TicketOrder values(0, 'Joe Smith', 0, '2022-06-16','2022-09-01','19:00',10, 0, 90.0);
--- insert into TicketOrder values(0, 10, 80.0, 22039);
--- insert into TicketOrder values(0, 20, 150.0, 13026);
--- insert into TicketOrder values(0, 1, 9.0, 26260);
